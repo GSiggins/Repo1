65effede526bd3596cfd76b0b4bc2a7bc8cfed0f
GSiggins <gregory.siggins@gmail.com>
2022-04-29 19:07:22 -0700

    completed javascript homework, buttons functioning

--- JSPractice/javascript.js
+++ JSPractice/javascript.js
@@ -6,7 +6,7 @@ document.getElementById("button1").addEventListener("click", function(){
 
 document.getElementById("button2").addEventListener("click", function(){
 
-    document.getElementById("box").style.color = "blue";
+    document.getElementById("box").style.backgroundColor = "blue";
 
 });
 
@@ -18,8 +18,6 @@ document.getElementById("button3").addEventListener("click", function(){
 
 document.getElementById("button4").addEventListener("click", function(){
 
-    document.getElementById("box").
-    style.height = "150px"
-    style.opacity = "100%";
+    document.getElementById("box").style = "height:150px; width:150px; background-color:orange; margin:25px"
 
 });
