2621fefc1104f9ef3168582a786fa038a1da78a2
GSiggins <gregory.siggins@gmail.com>
2022-04-29 18:25:04 -0700

    pre-work homework, css, HTML fanpage, and JS practice

---
+++ JSPractice/javascript.js
@@ -0,0 +1,25 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "350px";
+
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.color = "blue";
+
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity = "40%";
+
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").
+    style.height = "150px"
+    style.opacity = "100%";
+
+});
